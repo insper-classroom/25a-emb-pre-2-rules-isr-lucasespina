dd1af414b71955918a965a58be49b6a0a78bd69b
Lucas Espina <lucaseqc@al.insper.edu.br>
2025-02-19 13:41:31 -0300

    arrumando variaveis

--- main_isr_practice.c
+++ main_isr_practice.c
@@ -2,17 +2,18 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-const int BTN_PIN_R = 28;
 
-volatile int btn_flag = 0;
 
 void btn_callback(uint gpio, uint32_t events) {
+
     if (events & GPIO_IRQ_EDGE_FALL) { 
         btn_flag = 1;
     }
 }
 
 int main() {
+    const int BTN_PIN_R = 28;
+    volatile int btn_flag = 0;
     stdio_init_all();
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
