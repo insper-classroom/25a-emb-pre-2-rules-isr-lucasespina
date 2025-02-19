4a95b45e200c8400627dc701d06cb4a6367ef102
Lucas Espina <lucaseqc@al.insper.edu.br>
2025-02-19 13:44:03 -0300

    tesntando dnv

--- main_isr_practice.c
+++ main_isr_practice.c
@@ -2,28 +2,24 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-
+const int BTN_PIN_R = 28;
+volatile int btn_flag = 0;
+volatile int capture_flag = 0;
 
 void btn_callback(uint gpio, uint32_t events) {
-
     if (events & GPIO_IRQ_EDGE_FALL) { 
         btn_flag = 1;
     }
 }
 
 int main() {
-    const int BTN_PIN_R = 28;
-    int btn_flag = 0;
     stdio_init_all();
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
     gpio_pull_up(BTN_PIN_R);
     gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
 
-    volatile int capture_flag = 0;
-
     while (1) {
-
         if (btn_flag) {
             capture_flag = 1;
             btn_flag = 0;
